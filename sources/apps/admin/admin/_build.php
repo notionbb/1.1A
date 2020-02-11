967204420345aae97a8b810ec3d460c4e336e924
Phillip Buffington <nationforums@gmail.com>
2020-02-11 05:07:03 -0500

    Update _build.php

--- sources/apps/admin/admin/_build.php
+++ sources/apps/admin/admin/_build.php
@@ -1,12 +1,12 @@
 <?php
 
 	/*===================================================
-	//	CipherPixel © All Rights Reserved
+	//	NotionBB Â© All Rights Reserved
 	//---------------------------------------------------
-	//	CP-Core
-	//		by cipherpixel.net
+	//	NBB-Core
+	//		by notionbb.com
 	//---------------------------------------------------
-	//	File created: November 05, 2014 
+	//	File Updated - 2-11-2020
 	//=================================================*/
 	
 	/**
